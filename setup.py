b6be10cf68a4383796658753f51d65e36a0a8b72
Alex Koshelev <daevaorn@gmail.com>
2011-12-14 13:39:00 +0300

    setup.py integration

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(
+    name='pingdjack',
+    version='0.1',
+    packages=[
+        'pingdjack',
+    ],
+
+    author='Ivan Sagalaev',
+    author_email='Maniac@SoftwareManiacs.org',
+    description='Library for sites made with Django that implements server and client parts of Pingback protocol',
+    url='https://github.com/isagalaev/pingdjack/',
+)
