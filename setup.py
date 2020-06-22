a0cb07818624c34822808296dd115ebf94e057b8
Ivan Sagalaev <maniac@softwaremaniacs.org>
2020-06-21 21:11:23 -0700

    Version 0.4

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(
     name='pingdjack',
-    version='0.3',
+    version='0.4',
     packages=[
         'pingdjack',
     ],
 
     author='Ivan Sagalaev',
-    author_email='Maniac@SoftwareManiacs.org',
+    author_email='maniac@softwaremaniacs.org',
     description='Library for sites made with Django that implements server and client parts of Pingback protocol',
     url='https://github.com/isagalaev/pingdjack/',
     install_requires = [
